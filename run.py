2d1a062ac37cd9c3777a5673f55c1db7130024ee
Alex Chang <chang_0728@163.com>
2026-01-15 23:29:47 +0800

    修改report路径

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ import pytest
 
 if __name__ == '__main__':
     pytest.main()
-    time.sleep(1)
+    # time.sleep(1)
     # 构建、输入allure测试报告
-    os.system("/opt/homebrew/bin/allure generate ./report/allure_result_temp -o ./report/allure_report --clean")
+    # os.system("/opt/homebrew/bin/allure generate ./report/allure_result_temp -o ./report/allure_report --clean")
     # 自动打开测试报告，并且生成局域网地址
     # os.system("allure open ./report/allure_report")
