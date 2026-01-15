e77d927a0d0d4761ba310845d18db155f8ef4907
Alex Chang <chang_0728@163.com>
2026-01-15 22:19:00 +0800

    update allure full path

--- run.py
+++ run.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     pytest.main()
     time.sleep(1)
     # 构建、输入allure测试报告
-    os.system("allure generate ./report/allure_result_temp -o ./report/allure_report --clean")
+    os.system("/opt/homebrew/bin/allure generate ./report/allure_result_temp -o ./report/allure_report --clean")
     # 自动打开测试报告，并且生成局域网地址
     # os.system("allure open ./report/allure_report")
